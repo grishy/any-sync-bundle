c9a2459700ab0a3af236dd68e46aa097909c6d22
Sergey G. <mailgrishy@gmail.com>
2025-01-29 22:30:20 +0400

    feat: create config on start if not exist

--- cmd/start.go
+++ cmd/start.go
@@ -19,11 +19,6 @@ import (
 	bundleNode "github.com/grishy/any-sync-bundle/node"
 )
 
-const (
-	configBundlePath = "./data/cfg/priv_bundle.yml"
-	configClientPath = "./data/cfg/pub_client.yml"
-)
-
 type node struct {
 	name string
 	app  *app.App
@@ -32,10 +27,16 @@ type node struct {
 func cmdStart(ctx context.Context) *cli.Command {
 	return &cli.Command{
 		Name:  "start",
-		Usage: "Stat bundle services",
+		Usage: "Start bundle services",
 		Flags: []cli.Flag{},
 		Action: func(cCtx *cli.Context) error {
 			log := logger.NewNamed("main")
+
+			initExternalAddrs := cCtx.StringSlice(fGlobalInitExternalAddrs)
+			initMongoURI := cCtx.String(fGlobalInitMongoURI)
+			initRedisURI := cCtx.String(fGlobalInitRedisURI)
+			cfgPath := cCtx.String(fGlobalBundleConfigPath)
+			storagePath := cCtx.String(fGlobalStoragePath)
 
 			// TODO: AppName global, AppName not working properly on app instance
 
@@ -46,14 +47,20 @@ func cmdStart(ctx context.Context) *cli.Command {
 
 			var cfgBundle *bundleCfg.Config
 			log.Info("loading config")
-			if _, err := os.Stat(configBundlePath); err == nil {
+			if _, err := os.Stat(cfgPath); err == nil {
 				log.Info("loaded existing config")
-				cfgBundle = bundleCfg.Read(configBundlePath)
+				cfgBundle = bundleCfg.Read(cfgPath)
 			}
 
 			log.Info("file not found, created new config")
-			// TODO Real data
-			cfgBundle = bundleCfg.CreateWrite(&bundleCfg.CreateOptions{})
+			// TODO Create if not exist
+			cfgBundle = bundleCfg.CreateWrite(&bundleCfg.CreateOptions{
+				CfgPath:       cfgPath,
+				StorePath:     storagePath,
+				MongoURI:      initMongoURI,
+				RedisURI:      initRedisURI,
+				ExternalAddrs: initExternalAddrs,
+			})
 
 			cfgNodes := cfgBundle.NodeConfigs()
 
@@ -61,7 +68,7 @@ func cmdStart(ctx context.Context) *cli.Command {
 			// mongoInit(ctx, cfgBundle.Nodes.Coordinator.MongoConnect)
 
 			// Dump client config
-			// TODO
+			// TODO if no exist create
 			// cfgBundle.ClientConfig(configClientPath)
 
 			fileStore := filepath.Join(cfgBundle.StoragePath, "storage-file")
@@ -87,35 +94,39 @@ func cmdStart(ctx context.Context) *cli.Command {
 			}
 
 			// Start all services
-			log.Info("⚡ Initiating service startup", zap.Int("count", len(apps)))
+			log.Info("Initiating service startup", zap.Int("count", len(apps)))
 
 			for _, a := range apps {
-				log.Info("▶️ Starting service", zap.String("name", a.name))
+				log.Info("▶ Starting service", zap.String("name", a.name))
 				if err := a.app.Start(ctx); err != nil {
-					log.Panic("❌ Service startup failed",
+					log.Panic("✗ Service startup failed",
 						zap.String("name", a.name),
 						zap.Error(err))
 				}
 
-				log.Info("✅ Service started successfully", zap.String("name", a.name))
+				log.Info("✓ Service started successfully", zap.String("name", a.name))
 			}
 
-			log.Info("🚀 Service startup complete.")
+			log.Info("↑ Service startup complete")
 
 			// wait exit signal
 			<-ctx.Done()
 
 			// Stop apps in reverse order
+			log.Info("⚡ Initiating service shutdown", zap.Int("count", len(apps)))
 			for _, a := range slices.Backward(apps) {
+				log.Info("▶ Stopping service", zap.String("name", a.name))
 				ctxClose, cancelClose := context.WithTimeout(context.Background(), 30*time.Second)
 				if err := a.app.Close(ctxClose); err != nil {
-					log.Error("close error", zap.String("name", a.name), zap.Error(err))
+					log.Error("✗ Service shutdown failed", zap.String("name", a.name), zap.Error(err))
+				} else {
+					log.Info("✓ Service stopped successfully", zap.String("name", a.name))
 				}
 
 				cancelClose()
 			}
 
-			log.Info("goodbye!")
+			log.Info("→ Goodbye!")
 			return nil
 		},
 	}
@@ -143,7 +154,7 @@ func printWelcome() {
 
 	for _, mod := range info.Deps {
 		if strings.HasPrefix(mod.Path, "github.com/anyproto/any-sync") {
-			fmt.Printf(" ▸ %s (%s)\n", mod.Path, mod.Version)
+			fmt.Printf(" ‣ %s (%s)\n", mod.Path, mod.Version)
 		}
 	}
 	fmt.Print(`
